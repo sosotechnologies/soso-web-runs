c5994fbd59d9d7fe4259f98a171a85f993bffb2a
Collins Afanwi <117014550+sosotechnologies@users.noreply.github.com>
2023-05-17 14:05:50 -0400

    Update env.py

--- official-sosotech-web/env.py
+++ official-sosotech-web/env.py
@@ -1,16 +1,1 @@
-import os
-
-os.environ.setdefault("IP", "0.0.0.0")
-os.environ.setdefault("PORT", "5000")
-os.environ.setdefault("SECRET_KEY", "yz6{Teyyvejqfij{TQx;s,Z#O7g~")
-
-# MYSQL Database
-os.environ.setdefault("SQLALCHEMY_DATABASE_URI", "mysql+pymysql://root:Depay200$@localhost/sosotech")
-
-# Gmail Configuration
-os.environ.setdefault("MAIL_SERVER", "smtp.gmail.com")
-os.environ.setdefault("MAIL_PORT", "587")
-os.environ.setdefault("MAIL_USERNAME", "sosotechglobal@gmail.com")
-os.environ.setdefault(
-    "MAIL_PASSWORD", "tthtyfshxjvwacsm")
 
